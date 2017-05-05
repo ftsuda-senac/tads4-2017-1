14f93dede816e005623caafab660ed8ce17f7af9
Fernando Tsuda <ftsuda.senac@gmail.com>
2017-05-04 21:59:38 -0300

    Escolha da linguagem

--- cakeweb-jsf/src/main/java/br/senac/tads4/cakeweb/UsuarioBean.java
+++ cakeweb-jsf/src/main/java/br/senac/tads4/cakeweb/UsuarioBean.java
@@ -9,7 +9,11 @@ import br.senac.tads4.cakeweb.entidade.UsuarioSistema;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -19,21 +23,35 @@ import javax.servlet.http.HttpServletRequest;
 @Named
 @SessionScoped
 public class UsuarioBean implements Serializable {
-  
+
+  private static final Map<String, String> IDIOMAS;
+
+  static {
+    IDIOMAS = new LinkedHashMap<String, String>();
+    IDIOMAS.put("Inglês", "en-US");
+    IDIOMAS.put("Portugues", "pt-BR");
+  }
+
   private UsuarioSistema usuario = null;
-  
-  /** Nome de usuário informado */
+
+  /**
+   * Nome de usuário informado
+   */
   private String nomeUsuario;
-  
-  /** Senha informada */
+
+  /**
+   * Senha informada
+   */
   private String senha;
+
+  private String localeCode;
 
   /**
    * Creates a new instance of UsuarioBean
    */
   public UsuarioBean() {
   }
-  
+
   public String autenticar() {
     usuario = UsuarioSistema.obterUsuario(nomeUsuario, senha);
     if (usuario != null) {
@@ -41,10 +59,9 @@ public class UsuarioBean implements Serializable {
     }
     return "/erro-login.xhtml?faces-redirect=true";
   }
-  
+
   public String sair() {
-    HttpServletRequest req = (HttpServletRequest) 
-	    FacesContext.getCurrentInstance()
+    HttpServletRequest req = (HttpServletRequest) FacesContext.getCurrentInstance()
 	    .getExternalContext().getRequest();
     req.getSession().invalidate();
     return "/index.xhtml?faces-redirect=true";
@@ -73,5 +90,34 @@ public class UsuarioBean implements Serializable {
   public void setSenha(String senha) {
     this.senha = senha;
   }
-  
+
+  public String getLocaleCode() {
+    return localeCode;
+  }
+
+  public void setLocaleCode(String localeCode) {
+    this.localeCode = localeCode;
+  }
+
+  public Map<String, String> getCountriesInMap() {
+    return IDIOMAS;
+  }
+
+  /* Referencia: https://www.mkyong.com/jsf2/jsf-2-internationalization-example/ */
+  public void mudarIdioma(ValueChangeEvent e) {
+
+    String newLocaleValue = e.getNewValue().toString();
+
+    //loop country map to compare the locale code
+    for (Map.Entry<String, String> entry : IDIOMAS.entrySet()) {
+
+      if (entry.getValue().equals(newLocaleValue)) {
+	FacesContext.getCurrentInstance()
+		.getViewRoot()
+		.setLocale(Locale.forLanguageTag(entry.getValue()));
+
+      }
+    }
+  }
+
 }
